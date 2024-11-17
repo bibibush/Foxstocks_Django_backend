f0146e79078e8c28c1bb92070b3a0c5ff0d08d3b
Bibibush <rlawlsgus125@gmail.com>
2024-11-18 02:26:02 +0900

    drf 퍼미션 공부

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,7 +35,7 @@ router = routers.DefaultRouter()
 router.register(r"users",UserViewSet)
 
 urlpatterns = [
-    path("user/",include(router.urls)),
+    path("api/",include(router.urls)),
     path('admin/', admin.site.urls),
     path("api-auth",include("rest_framework.urls"))
 ]
