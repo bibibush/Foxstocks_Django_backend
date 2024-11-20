cc13c3a1a107bf6b0308d613c43d14e6a0b52bae
Bibibush <rlawlsgus125@gmail.com>
2024-11-20 23:56:36 +0900

    csv로 데이터를 마이그레이션 할때, pk를 제일 마지막 +1을 하도록 하기

--- stocks/models.py
+++ stocks/models.py
@@ -19,6 +19,17 @@ class Stock(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self,*args,**kwargs):
+        if not self.pk:
+            last_object = Stock.objects.last()
+            if last_object:
+                last_pk = last_object.pk
+                self.pk = last_pk + 1
+                super().save(*args,**kwargs)
+            else:
+                super().save(*args,**kwargs)
+        else:
+            super().save(*args,**kwargs)
 
 class DomesticStockManager(models.Manager):
 
