ecbcefbd495a18fdddc7799e978e91adae050f31
Bibibush <rlawlsgus125@gmail.com>
2024-11-23 00:35:19 +0900

    url변경

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth",include("rest_framework.urls")),
-    path("stocks/", include("stocks.urls")),
-    path("users/",include("accounts.urls"))
+    path("api/stocks/", include("stocks.urls")),
+    path("api/users/",include("accounts.urls"))
 ]
