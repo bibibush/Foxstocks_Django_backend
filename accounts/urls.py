3cac12e90391f38c8112a79315655670fef8863b
bibibush_jinhyeong <rlawlsgus125@gmail.com>
2024-12-04 16:37:56 +0900

    커스텀 토큰 흭득 뷰 작성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from accounts.views import CSRFEnsureView
+from accounts.views import CSRFEnsureView, CustomTokenObtainPairView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("",CSRFEnsureView.as_view()),
-    path("token/",TokenObtainPairView.as_view()),
+    path("token/",CustomTokenObtainPairView.as_view()),
     path("token/refresh/",TokenRefreshView.as_view())
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,11 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from accounts.models import User
 
 
 # Create your views here.
@@ -9,4 +14,14 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 @method_decorator(ensure_csrf_cookie,name='dispatch')
 class CSRFEnsureView(View):
     def get(self,request, *args, **kwargs):
-        return JsonResponse(data={"result_message": "SUCESS"}, safe=True, status=200)
+        return JsonResponse(data={"result_message": "SUCCESS"}, safe=True, status=200)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        user_email = request.data.get("email")
+        data = dict(vars(User.objects.get(email=user_email)))
+        del data["_state"]
+        del data["password"]
+        response = super().post(request,*args,**kwargs)
+
+        return Response({**response.data,**data})
