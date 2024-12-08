5c03b2d0cfb84fcb5f02ae78be42daf5a8a9aef2
Bibibush <rlawlsgus125@gmail.com>
2024-12-08 22:45:00 +0900

    모델 구조 변경

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,12 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
 
 # Create your models here.
 
 class User(AbstractUser):
     email = models.EmailField(_("email address"),unique=True)
-    invested = models.ForeignKey("Invested",on_delete=models.CASCADE,null=True,blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
@@ -16,8 +15,6 @@ class User(AbstractUser):
         return self.email
 
 class Invested(models.Model):
-    total = models.PositiveBigIntegerField()
+    input = models.PositiveBigIntegerField()
     company = models.ForeignKey("stocks.Stock",on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.total
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
