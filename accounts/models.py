7abf9cbf0e51303a15cc507986b34a5b32eba24d
Bibibush <rlawlsgus125@gmail.com>
2024-12-08 22:25:55 +0900

    Invested 테이블 추가

--- accounts/models.py
+++ accounts/models.py
@@ -7,9 +7,17 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     email = models.EmailField(_("email address"),unique=True)
+    invested = models.ForeignKey("Invested",on_delete=models.CASCADE,null=True,blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.email
+
+class Invested(models.Model):
+    total = models.PositiveBigIntegerField()
+    company = models.ForeignKey("stocks.Stock",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.total
