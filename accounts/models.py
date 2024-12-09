5ae9e675955bc5ae8d1dacefcc3945463a27b75b
Bibibush <rlawlsgus125@gmail.com>
2024-12-09 17:23:46 +0900

    Invested모델에 constraints 적용

--- accounts/models.py
+++ accounts/models.py
@@ -18,3 +18,7 @@ class Invested(models.Model):
     input = models.PositiveBigIntegerField()
     company = models.ForeignKey("stocks.Stock",on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user","company"],name="accounts_invested_uniq")
+        ]
