644d8b222385c92fcf745338b17b90f9ee1b8ed5
Bibibush <rlawlsgus125@gmail.com>
2024-12-19 10:22:25 +0900

    월을 숫자요소로 반환

--- stocks/ChartDatas.py
+++ stocks/ChartDatas.py
@@ -4,7 +4,6 @@ dayData = [{"day":str(i),"price":random.randint(530,560)*100} for i in range(1,3
 
 weekData = [{"week":str(w), "price": random.randint(530,570)*100} for w in range(1,25 + 1)]
 
-months = ["Jan","Feb","Mar","Avr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-monthData = [{"month":m,"price":random.randint(530,580)*100} for m in months]
+monthData = [{"month":str(m),"price":random.randint(530,580)*100} for m in range(1,12 + 1)]
 
 yearData = [{"year": str(y), "price":random.randint(530,600)*100} for y in range(2014,2024 + 1)]
